9356ff277e79c5edc1f3f4871a70ebcfc7de7658
Kalail <kalail@ubuntu.(none)>
2013-02-17 22:18:30 -0600

    Make fab deploy commands smarter.

--- hypestarter/fabfile.py
+++ hypestarter/fabfile.py
@@ -1,51 +1,136 @@
 from fabric.api import *
+
+# Import settings
 import hypestarter.settings as settings
 
+cmd = {
+	'development': (
+		'd',
+		'dev',
+		'development'
+	),
 
-def push():
+	'production': ()
+}
+
+
+def push(environment=None):
 	"""
 	Push the app to heroku.
 	"""
+	if not environment:
+		print "No environment declared!"
+		return
 	# Push to heroku
-	local('git push heroku')
+	if environment in cmd['development']:
+		cmd_env = 'heroku'
+	elif environment in cmd['production']:
+		cmd_env = 'heroku-production'
+	else:
+		print "Incorrect environment declared!"
+		return
 
-def add_config_vars():
+	local('git push %s' % cmd_env)
+
+
+def add_config_vars(environment=None):
 	"""
 	Add given config variables as environment variables.
 	
 	"""
-	config_vars = settings.CONFIG_VARS['DEVELOPMENT']
+	if not environment:
+		print "No environment declared!"
+		return
+
+	if environment in cmd['development']:
+		cmd_env = 'hypestarter-dev'
+		config_vars = settings.CONFIG_VARS['DEVELOPMENT']
+	elif environment in cmd['production']:
+		cmd_env = 'hypestarter'
+		config_vars = settings.CONFIG_VARS['PRODUCTION']
+	else:
+		print "Incorrect environment declared!"
+		return
+
 	config_commands = ['%s="%s"' % (name, var) for name, var in config_vars.items()]
 	config_command = ' '.join(config_commands)
-	local('heroku config:add %s --app hypestarter-dev' % config_command)
+	local('heroku config:add %s --app %s' % (config_command, cmd_env))
 
-def syncdb():
+def syncdb(environment=None):
 	"""
 	Run syncdb on the server.
 	
 	"""
-	local('heroku run python hypestarter/manage.py syncdb --app hypestarter-dev')
+	if not environment:
+		print "No environment declared!"
+		return
 
-def migrate():
+	if environment in cmd['development']:
+		cmd_env = 'hypestarter-dev'
+	elif environment in cmd['production']:
+		cmd_env = 'hypestarter'
+	else:
+		print "Incorrect environment declared!"
+		return
+
+	local('heroku run python hypestarter/manage.py syncdb --app %s' % cmd_env)
+
+def migrate(environment=None):
 	"""
 	Migrate the database on the server.
 	
 	"""
-	local('heroku run python hypestarter/manage.py migrate --app hypestarter-dev')
+	if not environment:
+		print "No environment declared!"
+		return
 
-def collectstatic():
-	local('heroku run python hypestarter/manage.py collectstatic --app hypestarter-dev')
+	if environment in cmd['development']:
+		cmd_env = 'hypestarter-dev'
+	elif environment in cmd['production']:
+		cmd_env = 'hypestarter'
+	else:
+		print "Incorrect environment declared!"
+		return
+
+	local('heroku run python hypestarter/manage.py migrate --app %s' % cmd_env)
+
+def collectstatic(environment=None):
+	if not environment:
+		print "No environment declared!"
+		return
+
+	if environment in cmd['development']:
+		cmd_env = 'hypestarter-dev'
+	elif environment in cmd['production']:
+		cmd_env = 'hypestarter'
+	else:
+		print "Incorrect environment declared!"
+		return
+
+	local('heroku run python hypestarter/manage.py collectstatic --app %s' % cmd_env)
 
 
-def deploy():
+def deploy(environment=None):
 	"""
 	Fully deploy the app to heroku, running all the steps.
 	"""
-	push()
-	add_config_vars()
-	syncdb()
-	migrate()
-	collectstatic()
+	if not environment:
+		print "No environment declared!"
+		return
+
+	if environment in cmd['development']:
+		env_name = 'development'
+	elif environment in cmd['production']:
+		env_name = 'production'
+	else:
+		print "Incorrect environment declared!"
+		return
+
+	push(env_name)
+	add_config_vars(env_name)
+	syncdb(env_name)
+	migrate(env_name)
+	collectstatic(env_name)
 
 
 def compile_coffee():
